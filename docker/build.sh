e9aa26e4082371c91ee607688f681f5fe9e4b522
Álvaro Arranz <aarranz@ficodes.com>
2019-04-12 19:17:48 +0200

    Extend available docker tags

--- docker/build.sh
+++ docker/build.sh
@@ -1,4 +1,20 @@
-docker build -t fiware/ckan-extended:2.8 .
-docker build -t fiware/ckan-extended:2.7 . -f Dockerfile-2.7
+version="7.7"
+full_version="${version}.0"
 
-docker tag fiware/ckan-extended:2.8 fiware/ckan-extended:latest
+docker build --squash -t fiware/ckan-extended:2.8-${full_version} .
+docker build --squash -t fiware/ckan-extended:2.7-${full_version} . -f Dockerfile-2.7
+
+docker tag fiware/ckan-extended:2.8-${full_version} fiware/ckan-extended:2.8-${version}
+docker tag fiware/ckan-extended:2.7-${full_version} fiware/ckan-extended:2.7-${version}
+docker tag fiware/ckan-extended:2.8-${full_version} fiware/ckan-extended:${full_version}
+docker tag fiware/ckan-extended:2.8-${full_version} fiware/ckan-extended:${version}
+docker tag fiware/ckan-extended:2.8-${version} fiware/ckan-extended:latest
+
+
+docker push fiware/ckan-extended:2.8-${full_version}
+docker push fiware/ckan-extended:2.7-${full_version}
+docker push fiware/ckan-extended:2.8-${version}
+docker push fiware/ckan-extended:2.7-${version}
+docker push fiware/ckan-extended:${full_version}
+docker push fiware/ckan-extended:${version}
+docker push fiware/ckan-extended:latest
